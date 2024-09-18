9be7947a3a1df5706b8299c9c4d345f41cc697b4
Nathan-47 <nathan.parchment47@gmail.com>
2024-09-18 21:50:07 +0100

    nav jsx file created

---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+
+const Navbar = () => {
+
+return (
+<div className="nav">
+    <ul>
+        <a href="#"><li className="nav-sign">Sign Up</li></a>
+        <a href="#"><li className="nav-login">Login</li></a>
+    </ul>
+</div>
+)
+};
+
+export default Navbar;
