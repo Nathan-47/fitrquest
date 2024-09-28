7feaec3f7c445f10106dca77d81c24c5b92f096c
Nathan-47 <nathan.parchment47@gmail.com>
2024-09-28 20:56:35 +0100

    about link moved to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,7 +6,10 @@ return (
 <header className="header">
     <ul>
         <a href="#"><li className="nav-sign">Sign Up</li></a>
-        <a href="#"><li className="nav-login">Login</li></a>
+        <a href="#"><li className="nav-login">Login</li>
+        </a>
+        <a href="#"><li className="nav-login">About</li>
+        </a>
     </ul>
 </header>
 )
