0da47c27467495782dcf95f6ccabd99785e0ca77
Nathan-47 <nathan.parchment47@gmail.com>
2024-09-28 20:56:58 +0100

    landing content updated

--- src/components/RevealBento.jsx
+++ src/components/RevealBento.jsx
@@ -1,10 +1,9 @@
-
+"use strict";
 
 import React from 'react';
 import { motion } from "framer-motion";
 import { twMerge } from "tailwind-merge";
-// import { FiArrowRight, FiMail, FiMapPin } from "react-icons/fi";
-// import { SiGithub, SiTiktok, SiTwitter, SiYoutube } from "react-icons/si";
+import journey1 from '../images/journey_pic.png';
 
 
 const RevealBento = () => {
@@ -13,15 +12,13 @@ const RevealBento = () => {
         initial="initial"
         animate="animate"
         transition={{
-          staggerChildren: 0.05,
+          staggerChildren: 0.08,
         }}
         className="mx-auto grid max-w-4xl grid-flow-dense grid-cols-12 gap-4"
       >
         <HeaderBlock />
-        <SocialsBlock />
+        <QuestBlock />
         <AboutBlock />
-        <LocationBlock />
-        <EmailListBlock />
       </motion.div>
   );
 };
@@ -57,147 +54,52 @@ const Block = ({ className, ...rest }) => {
 };
 
 const HeaderBlock = () => (
-  <Block className="col-span-12 row-span-2 md:col-span-6">
-    <h1 className="mb-12 text-4xl font-medium leading-tight">
-      Hi, I'm Tom.{" "}
-      <span className="text-zinc-400">
-        I build cool websites like this one.
-      </span>
-    </h1>
-    <a
-      href="#"
-      className="flex items-center gap-1 text-red-300 hover:underline"
-    >
-      Contact me 
-      {/* <FiArrowRight /> */}
+  <Block 
+  whileHover={{
+    scale: 1.03,
+  }}
+  className="col-span-12 row-span-1 md:col-span-6" id="landing-card">
+    <p className='landing-title'>No Idea, Get Fit Here</p>
+    <a 
+    href="#" 
+    className="flex items-center gap-1">Contact me 
     </a>
   </Block>
 );
 
-const SocialsBlock = () => (
+const QuestBlock = () => (
   <>
     <Block
       whileHover={{
-        rotate: "2.5deg",
-        scale: 1.1,
+        scale: 1.03,
       }}
-      className="col-span-6 bg-red-500 md:col-span-3"
+      className="col-span-6 row-span-2 md:col-span-6" id="quest-card"
     >
-      <a
-        href="#"
-        className="grid h-full place-content-center text-3xl text-white"
-      >
-        {/* <SiYoutube /> */}
-      </a>
-    </Block>
-    <Block
-      whileHover={{
-        rotate: "-2.5deg",
-        scale: 1.1,
-      }}
-      className="col-span-6 bg-green-600 md:col-span-3"
-    >
-      <a
-        href="#"
-        className="grid h-full place-content-center text-3xl text-white"
-      >
-        {/* <SiGithub /> */}
-      </a>
-    </Block>
-    <Block
-      whileHover={{
-        rotate: "-2.5deg",
-        scale: 1.1,
-      }}
-      className="col-span-6 bg-zinc-50 md:col-span-3"
-    >
-      <a
-        href="#"
-        className="grid h-full place-content-center text-3xl text-black"
-      >
-        {/* <SiTiktok /> */}
-      </a>
-    </Block>
-    <Block
-      whileHover={{
-        rotate: "2.5deg",
-        scale: 1.1,
-      }}
-      className="col-span-6 bg-blue-500 md:col-span-3"
-    >
-      <a
-        href="#"
-        className="grid h-full place-content-center text-3xl text-white"
-      >
-        {/* <SiTwitter /> */}
+      <img className="journey-hero" src={journey1} />
+      <p className='quest-title'>Take on the quest to fitness!</p>
+      <a 
+      href="#" 
+      className="flex items-center gap-1 text-black-800 hover:underline"> Find your quest
       </a>
     </Block>
   </>
 );
 
 const AboutBlock = () => (
-  <Block className="col-span-12 text-3xl leading-snug">
-    <p>
-      My passion is building cool stuff.{" "}
-      <span className="text-zinc-400">
-        I build primarily with React, Tailwind CSS, and Framer Motion. I love
-        this stack so much that I even built a website about it. I've made over
-        a hundred videos on the subject across YouTube and TikTok.
-      </span>
-    </p>
+  <>
+  <Block 
+    whileHover={{
+    scale: 1.03,
+    }}
+  className="col-span-6 text-3x1" id="landing-card">
+    <p className='comp-title'>Questcomp</p>
+    <p className='comp-text'>Get involved to win various prizes for your journey</p>
+    <a 
+      href="#" 
+      className="flex items-center gap-1 text-black-800 hover:underline"> Click to see more
+      </a>
   </Block>
+  </>
 );
-
-const LocationBlock = () => (
-  <Block className="col-span-12 flex flex-col items-center gap-4 md:col-span-3">
-    <div className="text-3xl" />
-    <p className="text-center text-lg text-zinc-400">Cyberspace</p>
-  </Block>
-);
-
-const EmailListBlock = () => (
-  <Block className="col-span-12 md:col-span-9">
-    <p className="mb-3 text-lg">Join my mailing list</p>
-    <form
-      onSubmit={(e) => e.preventDefault()}
-      className="flex items-center gap-2"
-    >
-      <input
-        type="email"
-        placeholder="Enter your email"
-        className="w-full rounded border border-zinc-700 bg-zinc-800 px-3 py-1.5 transition-colors focus:border-red-300 focus:outline-0"
-      />
-      <button
-        type="submit"
-        className="flex items-center gap-2 whitespace-nowrap rounded bg-zinc-50 px-3 py-2 text-sm font-medium text-zinc-900 transition-colors hover:bg-zinc-300"
-      >
-        {/* <FiMail />  */}
-        Join the list
-      </button>
-    </form>
-  </Block>
-);
-
-const Logo = () => {
-  return (
-    <svg
-      width="40"
-      height="auto"
-      viewBox="0 0 50 39"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className="mx-auto mb-12 fill-zinc-50"
-    >
-      <path
-        d="M16.4992 2H37.5808L22.0816 24.9729H1L16.4992 2Z"
-        stopColor="#000000"
-      ></path>
-      <path
-        d="M17.4224 27.102L11.4192 36H33.5008L49 13.0271H32.7024L23.2064 27.102H17.4224Z"
-        stopColor="#000000"
-      ></path>
-    </svg>
-  );
-};
 
 export default RevealBento;
