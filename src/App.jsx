eb75a4c4dbec96e2737233128ee69739ae032b21
Nathan-47 <nathan.parchment47@gmail.com>
2024-10-15 21:26:14 +0100

    react router function added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
+import {createBrowserRouter, Route, createRoutesFromElements, RouterProvider} from 'react-router-dom';
 import Navbar from "./components/navbar";
 import Landing from "./pages/landing";
+import About from "./pages/about";
 
 function App() {
 
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Navbar />}>  
+      <Route index element={<Landing/>} />
+
+      <Route path="about" element={<About/>} />
+
+      {/* 404 page link */}
+      {/* <Route path="*" element={<NotExist />} /> */}
+      </Route>
+    )
+  );
+
+
   return (
-    <>
-      <Navbar />
-      <Landing />
-    </>
-  )
+    <RouterProvider router={router} />
+  );
 }
 
 export default App;
