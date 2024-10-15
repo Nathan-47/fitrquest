6ac26b3c371a4fdf83e774835bc1cf985c5c33f5
Nathan-47 <nathan.parchment47@gmail.com>
2024-10-15 21:26:47 +0100

    NavLink import added

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -2,11 +2,13 @@
 
 import React from 'react';
 import RevealBento from '../components/RevealBento';
+import Navbar from '../components/navbar';
 
 const Landing = () => {
 
     return (
         <div className='bento-grid'>
+        {/* <Navbar/> */}
         <RevealBento />
         </div>
     );
