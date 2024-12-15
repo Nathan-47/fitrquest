43e4b0502ed952f48c34f40c9b24ec367a8124bf
Nathan-47 <nathan.parchment47@gmail.com>
2024-12-15 23:38:25 +0000

    useeffect function to retreive data from local database

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,14 +1,48 @@
 "use strict";
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 
 const About = () => {
-    return (
-        <>
-        <h1>Hello Me</h1>
-        </>
-    )
-}
+  const [books, setBooks] = useState([])
 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/getBook')
+      .then(books => setBooks(books.data))
+      .catch(err => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Pages</th>
+          </tr>
+        </thead>
+        <tbody>
+  {
+    
+    books.map((book) => {
+        console.log(books)
+      return (
+        <tr>
+          <td>{book.title}</td>
+          <td>{book.author}</td>
+          <td>{book.pages} {book.page}</td>
+        </tr>
+      );
+    })
+  }
+</tbody>
+
+      </table>
+    </div>
+  );
+  
+};
 
 export default About;
