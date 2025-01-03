8760e73d1e50410115f8534d339431a020a6bd75
Nathan-47 <nathan.parchment47@gmail.com>
2025-01-03 20:12:38 +0000

    json filtering test

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import bookModel from '../server/models/users.js'
-
+import bookModel from '../server/models/users.js';
+import { ObjectId } from 'mongodb';
 
 
 const app = express()
@@ -11,10 +11,36 @@ app.use(express.json())
 
 mongoose.connect('mongodb://localhost:27017/bookstore')
 
-app.get('/getBook', (req, res) => {
-    bookModel.find()
+// routes
+app.get('/books', (req, res) => {
+
+    bookModel.find() 
+    .sort({ author: 1 })
     .then(books => res.json(books))
-    .catch(err => res.json(err))
+    .catch(() => {
+        res.status(500).json({error: 'Could not fectch the documents'})
+    })
+})
+
+
+
+// FINDING SINGLE DOCUMENTS //
+app.get('/books/:id', (req, res) => {
+
+    // If the id string is valid then execute the filtering
+    if (ObjectId.isValid(req.params.id)) {
+
+    // req.params.id allows access to what is within the URL
+    bookModel.findOne({_id: new ObjectId(req.params.id)}) // find doc associated with ID
+    .then(doc => {
+        res.status(200).json(doc)
+    })
+    .catch(() => {
+        res.status(500).json({error: 'Could not fetch the documents'})
+    })
+    } else {
+        res.status(500).json({error: 'Not valid ID'})
+    }
 })
 
 
