fe2428b537f0ef3423678182f018499f3e50211c
Nathan-47 <nathan.parchment47@gmail.com>
2025-01-04 01:07:44 +0000

    updated property names for postman handling

--- server/models/users.js
+++ server/models/users.js
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
 
 const bookSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    age: Number
+    title: String,
+    author: String,
+    pages: Number,
+    rating: Number,
+    genres: Array,
+    review: Array,
+    name: String
+    // email: String,
+    // age: Number
 })
 
 const bookModel = mongoose.model('books', bookSchema)
