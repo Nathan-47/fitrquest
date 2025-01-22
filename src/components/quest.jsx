63c23a486a3f9bf4946e4a8acd6b40ee44521e84
Nathan-47 <nathan.parchment47@gmail.com>
2025-01-22 22:04:20 +0000

    added quest data

---
+++ src/components/quest.jsx
@@ -0,0 +1,11 @@
+const Quest = [
+    {
+        question: "I am",
+        answerA: "Agree",
+        idNextQuestionA: 1,
+        answerB: "Disagree",
+        idNextQuestionb: 14
+    },
+]
+
+export default Quest;
