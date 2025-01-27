a49f0f3fd4269f6b57d9aebf20d8cc522edf012e
Nathan-47 <nathan.parchment47@gmail.com>
2025-01-27 19:14:09 +0000

    youtube video embed via template literal

--- src/pages/questFinder.jsx
+++ src/pages/questFinder.jsx
@@ -2,12 +2,16 @@ import { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 
 
-// component
+// quest component data
 import Quest from "../components/quest";
 
 
-
 const QuestFinder = () => {
+
+// Query selectors
+const showImg = document.querySelector('.quest-image');
+const showVids = document.querySelector('.video1')
+
 
     let navigate = useNavigate();
     const [currentQ, setCurrentQ] = useState('')
@@ -24,6 +28,8 @@ const QuestFinder = () => {
             if (nextQ) {
                 setCurrentQ(nextQ)
             } else {
+
+                // FIXME: I dont know if this is needed?
                 navigate(`/result/${currentQ.questResultA}`)
             }
     }
@@ -34,19 +40,47 @@ const QuestFinder = () => {
         if (nextQ) {
             setCurrentQ(nextQ)
         } else {
+
+            // FIXME: I dont know if this is needed?
             navigate(`/result/${currentQ.questResultB}`)
         }
     }
 
 
+    // show the hidden image when answers are shown
+    console.log(currentQ.question)
+    if (currentQ.question === 'Bodybuilding') {
+        console.log('update me!')
+        showImg.classList.add('show');
+        showVids.classList.add('show');
+    }
+
+    // Display youtube vid per answer 
+    const ytvid = `https://www.youtube.com/embed/${currentQ.answerVideo}`
+    console.log(currentQ.answerVideo);
+
 
     return (
-        <>        
-        
-        <p>{currentQ.question}</p>
+        <>
 
-        <button className="quiz-button" onClick={selectClickA}> {currentQ.answerA}
+        <div className="textsection">{currentQ.question}</div>
+
+        <button className="quiz-button" onClick={selectClickA}> 
+        {currentQ.answerA}
+        <br />
+        <img className="quest-image" src={currentQ.imgFile} alt="bodybulding" />
+        <br />
+        <div className="quest-cal">{currentQ.answerCalories}</div>
+        <br />
+        <div className="quest-info">{currentQ.answerInfo}</div>
+        <br />
+
+        {/* TODO: For vids add the hidden class on divs instead of CSS */}
+        <div className="video1">
+            `<iframe id="video-frame" src={ytvid}></iframe>`
+            </div>
         </button>
+
 
         <button className="quiz-button" onClick={selectClickB}> {currentQ.answerB}
         </button>
@@ -54,8 +88,7 @@ const QuestFinder = () => {
         <br /><br />
 
 
-        {/* TODO: Link back to homepage to test if it works */}
-
+        {/* FIXME: Link back to homepage to test if it works */}
         <Link className="quiz-link" to="/" onClick={() => window.location.reload()}>Restart Quest</Link>
 
         </>
