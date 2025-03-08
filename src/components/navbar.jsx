eb5b8672fee1c39e57941e57eeba42c6a23e54e9
Nathan-47 <nathan.parchment47@gmail.com>
2025-03-08 21:31:39 +0000

    user.email updated to user.name to display name instead

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,44 @@
 "use strict";
 import { NavLink, Outlet } from "react-router-dom"; 
+import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Navbar = () => {
+
+const {logout} = useLogout();
+const {user} = useAuthContext();
+
+const handleClick = () => {
+    logout()
+}
+
 
 return (
     <header className="header">
     <ul>
-        <li className="nav-sign">
-            <NavLink to="#">Login</NavLink>
-            </li>
-
         <li className="nav-login">
             <NavLink to="About">About</NavLink>
-            </li>
+        </li>
+
+
+        {/* If not null then display user email */}
+        {user && ( <li className="nav-login">
+            <span>Welcome {user.name}</span>
+            <button onClick={handleClick}>Log out</button>
+        </li>
+        )}
+
+        {!user && (
+            <>
+        <li className="nav-sign">
+            <NavLink to="/login">Login</NavLink>
+        </li>
+
+        {/* <li className="nav-sign">
+            <NavLink to="/signup">Signup</NavLink>
+        </li> */}
+        </>
+        )}
     </ul>    
     
     <Outlet />
