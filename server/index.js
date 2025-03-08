32d41f9767a1c1592db5ea47f6f120b524cd487b
Nathan-47 <nathan.parchment47@gmail.com>
2025-03-08 21:33:11 +0000

    protected routes added for competition page auth

--- server/index.js
+++ server/index.js
@@ -3,15 +3,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import bookModel from '../server/models/users.js';
-import { ObjectId } from 'mongodb';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.js';
+import protectedRoutes from './routes/protectedRoutes.js';
 
-dotenv.config();  // Configure dotenv to load the .env file
 
-
+// Configure dotenv to load the .env file
+dotenv.config();  
 
 const app = express()
 app.use(cors());
@@ -21,6 +20,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // for user login or signup
 app.use('/user', userRoutes);
+
+// for protected pages in this case the /comp page
+app.use('/user/protected', protectedRoutes);
 
 
 // connect to database
@@ -32,105 +34,4 @@ mongoose.connect(process.env.MONGO_URI)
 })
 .catch((error) => {
     console.log(error)
-})
-
-
-
-
-// routes //
-app.get('/books', (req, res) => {
-
-    // Helpful API Pagination 
-    // Create a query parameter for pagination
-    const page = req.query.p || 0
- 
-    const booksPerPage = 3
-
-    bookModel.find() 
-    .sort({ author: 1 })
-    .skip(page * booksPerPage)
-    .limit(booksPerPage)
-    .then(books => res.json(books))
-    .catch(() => {
-        res.status(500).json({error: 'Could not fectch the documents'})
-    })
-})
-
-
-
-//////////////////////////
-// FINDING SINGLE DOCUMENTS 
-//////////////////////////
-app.get('/books/:id', (req, res) => {
-
-    // If the id string is valid then execute the filtering
-    if (ObjectId.isValid(req.params.id)) {
-
-    // req.params.id allows access to what is within the URL
-    bookModel.findOne({_id: new ObjectId(req.params.id)}) // find doc associated with ID
-    .then(doc => {
-        res.status(200).json(doc)
-    })
-    .catch(() => {
-        res.status(500).json({error: 'Could not fetch the documents1'})
-    })
-    } else {
-        res.status(500).json({error: 'Not valid ID'})
-    }
-})
-
-
-app.post('/books', (req, res) => {
-    
-    const book = req.body
-
-    bookModel.create(book)
-    .then(result => {
-        res.status(201).json(result)
-    })
-    .catch(() => {
-        res.status(500).json({err: 'Could not create new document'})
-    })
-})
-
-
-
-app.delete('/books/:id', (req, res) => {
-    // Find the ID of the book and execute a delete function from databse
-    
-    if (ObjectId.isValid(req.params.id)) {
-
-        // if ID valid then delete the id 
-        bookModel.deleteOne({_id: new ObjectId(req.params.id)}) 
-        .then(result => {
-            res.status(200).json(result)
-        })
-        .catch(() => {
-            res.status(500).json({error: 'Could not delete the document'})
-        })
-        } else {
-            res.status(500).json({error: 'Not valid ID'})
-        }
-})
-
-
-// Helps modify any books in the database
-app.patch('/books/:id', (req, res) => {
-
-    // Will be an object with various fields that we want to update
-    const updates = req.body
-
-    if (ObjectId.isValid(req.params.id)) {
-
-        // Two arguments that will find the book and other to set the update
-        bookModel.updateOne({_id: new ObjectId(req.params.id)}, {$set: updates}) 
-        .then(result => {
-            res.status(200).json(result)
-        })
-        .catch(() => {
-            res.status(500).json({error: 'Could not update document'})
-        })
-        } else {
-            res.status(500).json({error: 'Not valid ID'})
-        }
 })
