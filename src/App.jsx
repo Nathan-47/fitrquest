b21389a0ea89d5ab282eb8a935a0f20abe31b4eb
Nathan-47 <nathan.parchment47@gmail.com>
2025-03-09 01:43:25 +0000

    protected route wrapped to authenticate competition page

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,17 @@ import Navbar from "./components/navbar";
 import Landing from "./pages/landing";
 import About from "./pages/about";
 import QuestFinder from './pages/questFinder';
+import Login from './pages/login';
+import Signup from './pages/signup';
+import Comp from './pages/Comp'
+import ProtectedRoute from './components/ProtectedRoute';
+import { useAuthContext } from './hooks/useAuthContext';
+import { Navigate } from 'react-router-dom';
 
 
 function App() {
+
+  const {user} = useAuthContext()
 
   const router = createBrowserRouter(
     createRoutesFromElements(
@@ -14,8 +22,19 @@ function App() {
 
       <Route path="about" element={<QuestFinder/>} />
 
+
+      <Route path="/comp" element={
+      <ProtectedRoute>
+      <Comp/>
+      </ProtectedRoute>
+      } />
+
       {/* 404 page link */}
       {/* <Route path="*" element={<NotExist />} /> */}
+
+      {/* // if no user then login if user is found then go to competition page */}
+      <Route path='/login' element={!user ? <Login /> : <Navigate to="/comp" />} />
+      <Route path='/Signup' element={<Signup />} />
       </Route>
     )
   );
