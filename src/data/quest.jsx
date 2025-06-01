54f3d58934ee53caef04f6e997d186b9e4240cf5
Nathan-47 <nathan.parchment47@gmail.com>
2025-06-01 01:30:56 +0100

    quest-cheer removed

--- src/data/quest.jsx
+++ src/data/quest.jsx
@@ -5,29 +5,78 @@ const Quest = [
         answerA: "20-30",
         idNextQuestionA: 20,
         answerB: "50+",
-        idNextQuestionB: 14
+        idNextQuestionB: 21
     },
     {
         question: "Bodybuilding",
         id: 10,
-        answerA: "You are young",
-        imgFile: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEfkwsry9xQQcpBp1Dlt6iYpp8H4UCVzKbPA&s',
-        answerInfo: 'Hello this is your body and you must eat and stick your diet',
-        answerVideo: 'HuXijX93w0o?si=pZx4gGJnCAL7FiO5'
     },
     {
-        question: "Your journey has led you to Powerlifting! 💪",
         id: 20,
-        answerIntro: "From your input, you have been seen fit to begin your powerlifting journey. We’ll provide a beginner orientated roadmap to help you get stronger, master the three main lifts, and optimise your training for strength gains. What You'll Learn: Training: Focus on the big three – the squat, deadlift, and bench press. We’ll guide you through structured programs and tips to perfect your technique and lift heavier. (See below for Powerlifting Influencers and vids)",
+        question: "Bodybuilding",
+        intro: "Your journey has led you to Bodybuilding! 💪",
+        content: "From your input, you have been seen fit to begin your bodybuilding journey. We’ll provide a beginner orientated roadmap to help you get stronger plus gain size and strength whilst looking good.",
 
-        answerFood: "Nutrition: Eat around 3,000Kcal use this calculator for help",
+        training: 'Focus on compound movements and full-body routines that target all major muscle groups. Using the program link below it allows you to see how the exercise are executed via video form.',
 
-        answerRec: "Recovery: Proper recovery is essential to progress in powerlifting. Click here [show the video from other data sections] to view video on recovery", 
+        nutrition: 'Use the calorie parameters as a guide to adjust your macro tracker if you want to using Marco Finder Link [1]. \n For an automated meal plan to fit your calorie intake use the Meal Plan link [2] to make meal planning healthier and easier.',
+
+        answerlink1: 'https://www.calculator.net/macro-calculator.html',
+        answerLink2: 'https://www.eatthismuch.com/',
+        answerLink3: 'tkt_ju3NoUw',
+
+
+        recovery: 'Proper recovery is essential to progress in bodybuilding. Please check out both videos that discuss DOMS recovery and a checklist of steps to recovery efficiently.',
+        recoveryLink: 'https://www.healthline.com/health/muscle-recovery#injury-prevention',
+        recoveryvid: 'PUJpU6Ti5jU',
+
+        learn: 'Here are steps on how to program your bodybuilding future \n\n 1. Understand Bodybuilding Basics\n The primary focus is performing exercises that target muscle all muscle groups, often with a higher volume of sets and reps. You must include key principles such as progressive overload, proper form, muscle recovery, and consistency. \n\n  2. Set Your Goals \n Establish both short and long-term goals to stay motivated and focused on your physique development for example gaining muscle mass, improving body composition, or enhancing strength. \n\n 3. Choose a Program \n Beginners: If you"re new to bodybuilding, focus on full-body workouts that target all major muscle groups 3 times per week. Examples are using StrongLifts 5x5 or Full-Body Workout Split. These routines help build foundational strength and muscle mass.. \n\n  4. Track Your Progress \n Monitor your progress by tracking key metrics such as weight lifted, number of sets, reps, body measurements, and body composition. Regularly adjust your workouts to ensure continuous progression, making sure to focus on muscle recovery to avoid overtraining. \n\n  5. Nutrition \n Fuel your body with a balanced diet rich in lean proteins (chicken, fish, tofu), complex carbohydrates (sweet potatoes, rice), and healthy fats (avocados, nuts). Ensure you"re eating enough to support muscle growth, aiming for a slight calorie surplus if bulking or maintaining a calorie deficit if cutting.',
         
-        answerLearn: `"Progress: Track your PRs (Personal Records), measure improvements, and modify your routine to break through lifting plateaus. (Check out the videos below for tips on progress tracking.) [Link the strength program website]"`, 
+        imgFile: 'src/images/influencers/sam_sulek.jpg',
+        imgFile2: 'src/images/influencers/jeff_nippard.jpg',
+        imgFile3: 'src/images/influencers/natasha_aughey.jpg',
 
-        imgFile: '',
+        imgFileName:'Sam Sulek',
+        imgFileName2:'Jeff Nippard',
+        imgFileName3:'Natasha Aughey',
+        
         answerInfo: '',
+
+        answerVideo: 'HuXijX93w0o?si=pZx4gGJnCAL7FiO5',
+    },
+    {
+        id: 21,
+        question: "Powerlifting",
+        intro: "Your journey has led you to Powerlifting! 💪",
+        content: "From your input, you have been seen fit to begin your powerlifting journey. We’ll provide a beginner orientated roadmap to help you get stronger, master the three main lifts, and optimise your training for strength gains.",
+
+        training: 'Focus on compound movements and full-body routines that target all major muscle groups.',
+
+        // TODO: Need to create a link same as the answerLinks below
+
+        nutrition: 'We recommend to eat 2,500Kcal - 3,000Kcal+ but you can adjust using the Marco Finder [1] to suit your daily routine. \n For an automated meal plan to fit your calorie intake use the Meal Plan link [2] to make meal planning healthier and easier. \n To track your calories and weight use our recommended app MyFitnessPal as it easy to use and navigate through for all levels [3].',
+
+        answerlink1: 'https://www.calculator.net/macro-calculator.html',
+        answerLink2: 'https://www.eatthismuch.com/',
+        answerLink3: 'tkt_ju3NoUw',
+
+
+        recovery: 'Proper recovery is essential to progress in Powerlifting. Please check out both videos that discuss DOMS recovery and a checklist of steps to recovery efficiently.',
+        recoveryLink: 'https://www.healthline.com/health/muscle-recovery#injury-prevention',
+        recoveryvid: 'PUJpU6Ti5jU',
+
+        learn: 'Here are steps on how to program your Powerlifting future \n\n 1. Understand Powerlifting Basics\n TPowerlifting focuses on three lifts: squat, bench press, and deadlift. Key principles include progressive overload, proper form, and consistency. \n\n  2. Set Your Goals \n Define short-term and long-term goals (e.g., increasing your 1RM). Use/research the SMART goal system to stay focused and motivated. \n\n 3. Choose a Program \n Start with beginner programs like Starting Strength for fundamental strength, or try 5/3/1 for flexibility as you progress. Advanced lifters can explore Westside Barbell for specialized methods. \n\n  4. Track Your Progress \n Track your lifts, volume, and intensity weekly. Adjust your training to ensure gradual improvement without overtraining. \n\n  5. Nutrition \n Fuel your body with a balanced diet rich in lean proteins (chicken, fish, tofu), complex carbohydrates (sweet potatoes, rice), and healthy fats (avocados, nuts). Ensure you"re eating enough to support muscle growth, aiming for a calorie surplus. \n\n 6. Sample Weekly Plan \n Beginners: Squat, bench, and deadlift (3x a week). \n Advanced: Max effort days for each lift, plus accessory work.',
+        
+        imgFile: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEfkwsry9xQQcpBp1Dlt6iYpp8H4UCVzKbPA&s',
+        imgFile2: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEfkwsry9xQQcpBp1Dlt6iYpp8H4UCVzKbPA&s',
+        imgFile3: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEfkwsry9xQQcpBp1Dlt6iYpp8H4UCVzKbPA&s',
+
+        imgFileName:'Sam Sulek',
+        imgFileName2:'',
+        imgFileName3:'',
+
+        answerInfo: '',
+
         answerVideo: 'HuXijX93w0o?si=pZx4gGJnCAL7FiO5',
     },
 ];
