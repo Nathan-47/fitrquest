03e2c160448fcc81d135ec58e30d958e62076fae
Nathan-47 <nathan.parchment47@gmail.com>
2025-06-01 22:12:00 +0100

    testing on page deleted

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,45 +1,10 @@
 "use strict";
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 
 
 const About = () => {
-  const [books, setBooks] = useState([])
-
-  useEffect(() => {
-    axios
-      .get('http://localhost:4000/books')
-      .then(books => setBooks(books.data))
-      .catch(err => console.log(err));
-  }, []);
-
   return (
     <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Author</th>
-            <th>Pages</th>
-          </tr>
-        </thead>
-        <tbody>
-  {
-    
-    books.map((book) => {
-        console.log(books)
-      return (
-        <tr>
-          <td>{book.title}</td>
-          <td>{book.imageFile}</td>
-          <td>{book.pages} {book.page}</td>
-        </tr>
-      );
-    })
-  }
-</tbody>
-
-      </table>
     </div>
   );
   
