1e0a87896a08838f7604936da15557aca8b9523b
Nathan-47 <nathan.parchment47@gmail.com>
2025-07-15 21:35:55 +0100

    added logo and fixed login btn alignment

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,24 +2,24 @@
 import { NavLink, Outlet } from "react-router-dom"; 
 import { useLogout } from "../hooks/useLogout";
 import { useAuthContext } from "../hooks/useAuthContext";
+import logo from "../images/fitrquest_icon.svg"
 
 const Navbar = () => {
 
 const {logout} = useLogout();
 const {user} = useAuthContext();
 
-const handleClick = () => {
-    logout()
+const handleClick = () => { 
+    logout() 
 }
 
 
 return (
     <header className="header">
     <ul>
-        <li className="nav-login">
-            <NavLink to="About">About</NavLink>
+        <li className="nav-logo">
+            <NavLink to="/"> <img src={logo} alt="fitrquest"/></NavLink>
         </li>
-
 
         {/* If not null then display user email */}
         {user && ( <li className="nav-login">
@@ -29,15 +29,9 @@ return (
         )}
 
         {!user && (
-            <>
-        <li className="nav-sign">
-            <NavLink to="/login">Login</NavLink>
-        </li>
-
-        {/* <li className="nav-sign">
-            <NavLink to="/signup">Signup</NavLink>
-        </li> */}
-        </>
+        <NavLink to="/login" className="nav-sign">
+            <li>Login</li>
+        </NavLink>       
         )}
     </ul>    
     
