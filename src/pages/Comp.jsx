16b7ed9d9e4871086316af4f47acf8f590579e4c
Nathan-47 <nathan.parchment47@gmail.com>
2025-07-26 00:20:10 +0100

    comp page content start layout

--- src/pages/Comp.jsx
+++ src/pages/Comp.jsx
@@ -1,8 +1,39 @@
+import compImg1 from '../images/competition/comp_rewards.png'
+import compImg2 from '../images/competition/comp_insta_stories.png'
+
+
 const Comp = () => {
     return (
-        <div>
-            <h1>Protected Page</h1>
-            <p>This page is only accessible to logged-in users.</p>
+        <div className="container">
+            <div className="row">
+                <div class="grid grid-cols-3 gap-4">
+                    <div class="col-span-1">
+                        <img src={compImg1} alt='#'/>
+                    </div>
+                    <div class="col-span-2">
+                        <h1>What can be won</h1>
+                        <p>Hey Quest taker, you can Participate in our FitrChallenge that will give you the chance to win various prizes. Start your FitrChallenge today and take advantage of these amazing rewards!</p>
+                        <ul className='list-disc'>
+                            <li>1 -year free subscriptions to JAAQ</li>
+                            <li>Raw Nutrition supplements: Whey protein, Creatine, Pre-workout</li>
+                            <li>Revive supplements: Collagen powder, Daily Greens powder</li>
+                        </ul>
+                    </div>
+                    
+                    <div class="col-span-2">
+                        <h1>How to win</h1>
+                        <p>There will be THREE  winners from three categories. How a winner is selected is from other Questers on what video or photo is their favourite for the month. To enter upload video or photo to any social media and tag the following  #FitrChallenge.</p>
+                    </div>
+                    <div class="col-span-1">
+                        <img src={compImg2} alt='#'/>
+                    </div>
+                </div>
+
+                {/* submit clip button */}
+                <div className='submit-wrapper'>
+                    <a href="#"><button className='submitBtn'>Submit clip</button></a>
+                </div>
+            </div>
         </div>
     );
 };
