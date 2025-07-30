0f71026f517a08b6066d72efc222c7cfc8833057
Nathan-47 <nathan.parchment47@gmail.com>
2025-07-30 20:41:50 +0100

    login handler

---
+++ src/hooks/useLogin.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { useAuthContext } from './useAuthContext';
+
+
+export const useLogin = () => {
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(null);
+    const { dispatch } = useAuthContext();
+
+    const login = async (email, password) => {
+        setIsLoading(true);
+        setError(null);
+
+        const response = await fetch('http://localhost:4000/user/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, password })
+        });
+
+        // try catch block to check whether the response body is empty or not valid JSON
+        let json;
+        try {
+            json = await response.json();
+        } catch (err) {
+            json = null;
+        }
+
+        // If response is not working then terminate loading and display error
+        if (!response.ok) {
+            setIsLoading(false);
+            setError(json ? json.error : 'Error');
+        }
+        if (response.ok) {
+            // save user to local storage
+            // TODO: Check to see if using local storage is secure for users
+            localStorage.setItem('user', JSON.stringify(json));
+
+            // update auth context
+            dispatch({ type: 'LOGIN', payload: json });
+            
+            setIsLoading(false);
+        }
+    };
+
+    return { login, isLoading, error };
+};
