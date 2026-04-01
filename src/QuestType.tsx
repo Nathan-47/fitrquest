703a1ca881549a83616581f98d57efcb96e79a2a
Nathan-47 <nathan.parchment47@gmail.com>
2026-04-01 23:09:09 +0100

    Added QuestType interface to define quest structure for questFinder file

---
+++ src/QuestType.tsx
@@ -0,0 +1,44 @@
+export interface QuestType {
+  id: number;
+  question: string;
+
+  answerA?: string;
+  answerB?: string;
+
+  idNextQuestionA?: number;
+  idNextQuestionB?: number;
+
+  intro?: string;
+  content?: string;
+
+  training?: string;
+  nutrition?: string;
+  recovery?: string;
+  learn?: string;
+
+  answerlink1?: string;
+  answerLink2?: string;
+  answerLink3?: string;
+
+  recoveryLink?: string;
+  recoveryvid?: string;
+  trainingLink?: string;
+
+  imgFile?: string;
+  imgFile2?: string;
+  imgFile3?: string;
+
+  imgFileName?: string;
+  imgFileName2?: string;
+  imgFileName3?: string;
+
+  altTag1?: string;
+  altTag2?: string;
+  altTag3?: string;
+
+  instaName?: string;
+  instaName2?: string;
+  instaName3?: string;
+
+  answerVideo?: string;
+}
