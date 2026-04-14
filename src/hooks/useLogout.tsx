e5fa16cdf9abcabfa82dbdcf93876fb80e50cfe0
Nathan-47 <nathan.parchment47@gmail.com>
2026-04-14 21:25:58 +0100

    Converted useLogout hook to TypeScript

---
+++ src/hooks/useLogout.tsx
@@ -0,0 +1,16 @@
+import { useAuthContext } from "./useAuthContext";
+
+export const useLogout = () => {
+  const { dispatch } = useAuthContext();
+
+  const logout = () => {
+    // terminate user from storage
+    localStorage.removeItem("user");
+
+    // dispatch logout action
+    //user will return null
+    dispatch({ type: "LOGOUT" });
+  };
+
+  return { logout };
+};
