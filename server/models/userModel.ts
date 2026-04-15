fa137273814a7b1f9b9918899c831ebb7a91e4f7
Nathan-47 <nathan.parchment47@gmail.com>
2026-04-15 20:14:47 +0100

    Refactored userModel to TypeScript with interfaces for User, UserDocument, and UserModel

--- server/models/userModel.ts
+++ server/models/userModel.ts
@@ -1,29 +1,50 @@
-import mongoose from "mongoose";
+import mongoose, { Schema, Model, Document } from "mongoose";
 import bcrypt from "bcrypt";
 import validator from "validator";
 
-const Schema = mongoose.Schema;
+interface IUser {
+  name: string;
+  email: string;
+  password: string;
+}
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+interface IUserDocument extends IUser, Document {
+  _id: mongoose.Types.ObjectId;
+}
+
+interface IUserModel extends Model<IUserDocument> {
+  signup(name: string, email: string, password: string): Promise<IUserDocument>;
+  login(email: string, password: string): Promise<IUserDocument>;
+}
+
+const userSchema = new Schema<IUserDocument, IUserModel>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 // Sign up static method
 // create a method that we can call to signup new user for database
-userSchema.statics.signup = async function (name, email, password) {
-  // user validation //
+userSchema.statics.signup = async function (
+  name: string,
+  email: string,
+  password: string
+): Promise<IUserDocument> {
   if (!name || !email || !password) {
     throw Error("All fields must be filled");
   }
@@ -33,39 +54,35 @@ userSchema.statics.signup = async function (name, email, password) {
   if (!validator.isStrongPassword(password)) {
     throw Error("Password is weak");
   }
-  // user validation //
 
-  // if email exists then will return user email if not then null
-  // use "this" because there is no user data as this is the export file
   const exists = await this.findOne({ email });
 
   if (exists) {
     throw Error("Email already in use");
   }
 
-  // password hashing for encryption
   const modifyPass = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password, modifyPass);
 
-  // Take password and store it in user document
   const user = await this.create({ name, email, password: hash });
 
   return user;
 };
 
 // static login method
-userSchema.statics.login = async function (email, password) {
+userSchema.statics.login = async function (
+  email: string,
+  password: string
+): Promise<IUserDocument> {
   if (!email || !password) {
     throw Error("All fields must be filled");
   }
 
-  // want to find user based on email given return null if none is found
   const user = await this.findOne({ email });
   if (!user) {
     throw Error("Incorrect email");
   }
 
-  // Does password match to hash password
   const match = await bcrypt.compare(password, user.password);
 
   if (!match) {
@@ -75,5 +92,5 @@ userSchema.statics.login = async function (email, password) {
   return user;
 };
 
-const userModel = mongoose.model("User", userSchema);
+const userModel = mongoose.model<IUserDocument, IUserModel>("User", userSchema);
 export default userModel;
