a5e69f79f4a8595a9e0d94ed34076992b6111326
Nathan-47 <nathan.parchment47@gmail.com>
2026-04-15 20:13:33 +0100

    Converted protectedController.js to TypeScript

--- server/controllers/protectedController.js
+++
@@ -1,9 +0,0 @@
-export const getProtectedData = (req, res) => {
-    // Example protected data
-    const protectedData = {
-        message: "This is protected data",
-        user: req.user
-    };
-
-    res.status(200).json(protectedData);
-};
